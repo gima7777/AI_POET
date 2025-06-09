9c16f8d07c92b162c7b74df3d1348e91427d649a
gima7777 <wapon77@naver.com>
2025-06-09 09:34:28 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_openai import ChatOpenAI
+chat_model = ChatOpenAI()
+
+
+
+import streamlit as st
+st.title("인공지능 시인")
+subject= st.text_input("시의 주제를 입력하세요")
+st.write("시의 주제 : " + subject)
+
+result = chat_model.invoke(subject + "에 대한 시를 써줘")
+st.write(result.content)
