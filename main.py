e1e9a9347316981c3b05966be095251121679837
gima7777 <wapon77@naver.com>
2025-06-09 09:35:03 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ st.title("인공지능 시인")
 subject= st.text_input("시의 주제를 입력하세요")
 st.write("시의 주제 : " + subject)
 
-result = chat_model.invoke(subject + "에 대한 시를 써줘")
-st.write(result.content)
+if st.button("시 작성"):
+    with st.spinner("시 작성중..."):
+        result = chat_model.invoke(subject + "에 대한 시를 써줘")
+        st.write(result.content)
